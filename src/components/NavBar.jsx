a35e73e4fdfe74632a50c6b2b1b9688f7dec30d7
Velato <filippofornai1@gmail.com>
2026-08-16 15:01:19 +0200

    Added navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { navLinks } from '../constants'
+
+
+const NavBar = () => {
+  return (
+    <header>
+        <nav>
+            <img src={import.meta.env.BASE_URL+"logo.svg"} alt="Apple logo"/>
+            <ul>
+                {navLinks.map(({label})=>(
+                    <li key={label}>
+                        <a href={import.meta.env.BASE_URL+label}>{label}</a>
+                    </li>
+                ))}
+            </ul>
+
+            <div className='flex-center gap-3'>
+                <button>
+                    <img src={import.meta.env.BASE_URL+"search.svg"} alt="Search" />
+                </button>
+                <button>
+                    <img src={import.meta.env.BASE_URL+"cart.svg"} alt="Cart" />
+                </button>
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+export default NavBar
